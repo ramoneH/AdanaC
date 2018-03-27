0fa14a376b6a62bffe56ceb1cfd84b595df87324
enomar <ramonehamlett@gmail.com>
2018-03-27 15:17:58 -0500

    updates

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,21 @@
 var express = require('express');
 var router = express.Router();
-
+var mongo = require("mongodb").MongoClient;
+var objectID = require("mongodb").ObjectID;
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Task Tracker App' });
 });
 
 /* GET test page. */
-router.get('/test', function(req, res, next) {
-  res.render('test', { title: 'Task Tracker App' });
+router.get('/update', function(req, res, next) {
+    var db = req.db;
+    var collection = db.get('taskcollection');
+    collection.find({},{},function(e,docs){
+        res.render('update', {
+            "tasks" : docs
+        });
+    });
 });
 // /* Get Tasks Page */
 // router.get('/tasks', function(req, res) {
@@ -31,12 +38,63 @@ router.get('/addtasks', function(req, res) {
     res.render('addtasks', { title: 'Add New Tasks' });
 });
 
+
+/* GET UPDATE TASK page. */
+router.put('/updateTask', function(req, res) {
+    // Set our internal DB variable
+    var db = req.db;
+    // Set our collection
+    var collection = db.get('taskcollection');
+    // Get our form values. These rely on the "name" attributes
+
+    var task = req.body.taskname;
+    var desc = req.body.taskdesc;
+    var comp = req.body.taskcomp;
+    var ass = req.body.taskass;
+    var id = req.body._id;
+    collection.update({"_id": objectID(id)},{
+
+    },function(err, doc) {
+        
+        if (err) {
+            // If it failed, return error
+            res.send("There was a problem adding the information to the database.");
+        }
+        else {
+            // And forward to success page
+            res.redirect("tasks");
+
+        }
+    });
+});
+
+//     }
+
+//     // Submit to the DB
+//     collection.insert({"_id": objectID(id)},{
+//         "task" : task,
+//         "desc" : desc,
+//         "comp" : comp,
+//         "ass" : ass
+//     }, function (err, doc) {
+//         if (err) {
+//             // If it failed, return error
+//             res.send("There was a problem adding the information to the database.");
+//         }
+//         else {
+//             // And forward to success page
+//             res.redirect("tasks");
+//         }
+//     });
+//   });
+
+
 // POST to Add Task Service /
 router.post('/addtask', function(req, res) {
 
     // Set our internal DB variable
     var db = req.db;
-
+    
     // Get our form values. These rely on the "name" attributes
     var task = req.body.taskname;
     var desc = req.body.taskdesc;
@@ -46,6 +104,7 @@ router.post('/addtask', function(req, res) {
 
     // Set our collection
     var collection = db.get('taskcollection');
+
 
     // Submit to the DB
     collection.insert({
@@ -64,7 +123,5 @@ router.post('/addtask', function(req, res) {
         }
     });
 });
-function addnew() {
-  window.location.href = '/addtasks';
-}
+
 module.exports = router;
